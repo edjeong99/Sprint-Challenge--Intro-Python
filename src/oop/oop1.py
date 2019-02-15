ea4aafb4b057e8f931f3aa1d9c37d7738c0bdd96
Edward J <edjeong99@gmail.com>
2019-02-14 22:36:18 -0500

    create class heirachy

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,3 +21,27 @@
 
 class Vehicle:
     pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Car(Motorcycle):
+    pass
